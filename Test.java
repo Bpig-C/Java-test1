6b234929a715f813b430d5d2d40ac1960114e9db
Bpig-C <71739495+Bpig-C@users.noreply.github.com>
2020-10-07 08:11:36 +0800

    Make the date right.

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test {
 
    public static void main(String args[]) {
 
-       CPU cpu = new CPU(20,"Intel",3.8);
+       CPU cpu = new CPU(2200,"Intel",3.8);
 
        HardDisk HD=new HardDisk(200,240);
 
