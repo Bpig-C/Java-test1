cab4350e8d2f16cd5625489538a43c4ffb3c86ef
Bpig-C <71739495+Bpig-C@users.noreply.github.com>
2020-10-07 08:13:39 +0800

    Change the show().

--- PC.java
+++ PC.java
@@ -21,7 +21,8 @@ void setHardDisk(HardDisk h) {
 	HD=h;
 }
 void show() {
-	System.out.println("CPU type is "+cpu.getType()+".");
+	System.out.println("The PC attribute(404000 means wrong!):");
+	System.out.println("CPU type is "+cpu.getProcessor()+".");
 	System.out.println("CPU speed is "+cpu.getSpeed()+" MB/s.");
 	System.out.println("CPU hertz is "+cpu.getHertz()+" GHZ.");
 	System.out.println("HardDisk is "+HD.getAmount()+" G.");
